7ac986227490756bcd35bc359c5d6e1c5d257f6b
rayanadir <rayan.dahmena@gmail.com>
2022-01-21 12:14:47 +0100

    ajout label erreur extension

--- src/views/NewBillUI.js
+++ src/views/NewBillUI.js
@@ -2,7 +2,7 @@ import VerticalLayout from './VerticalLayout.js'
 
 export default () => {
 
-  return (`
+    return (`
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -56,6 +56,7 @@ export default () => {
                   <div class="col-half">
                     <label for="file" class="bold-label">Justificatif</label>
                     <input required type="file" class="form-control blue-border" data-testid="file" />
+                    <label id="required_extension" class="required_extension">Extension requise : png, jpg ou jpeg</label>
                   </div>
                 </div>
             </div>
