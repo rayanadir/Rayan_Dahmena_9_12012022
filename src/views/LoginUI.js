88bdf40b2809efea870414bac6fb0b059a113de5
rayanadir <88830520+rayanadir@users.noreply.github.com>
2022-01-28 16:53:44 +0100

    identifiants ajoutés à l'input

--- src/views/LoginUI.js
+++ src/views/LoginUI.js
@@ -1,7 +1,6 @@
-
 export default () => {
 
-  return(`
+    return (`
       <div class="page-div">
         <div class="row">
           <div class="title-container"> 
@@ -19,9 +18,9 @@ export default () => {
                 <form class="form-signin" data-testid="form-employee">
                   <h2 class="h3 mb-3 font-weight-normal">Employé</h1>
                   <label for="inputEmail">Votre email</label>
-                  <input type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
+                  <input type="email" value="employee@test.tld" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
                   <label for="inputPassword">Mot de passe</label>
-                  <input type="password" data-testid="employee-password-input" class="form-control" placeholder="******" required>
+                  <input type="password" value="employee" data-testid="employee-password-input" class="form-control" placeholder="******" required>
                   <button class="btn btn-lg btn-primary btn-block" data-testid="employee-login-button" style="background-color: #0E5AE5;" type="submit">Se connecter</button>
                 </form>
               </div>
@@ -33,9 +32,9 @@ export default () => {
                 <form class="form-signin" data-testid="form-admin">
                   <h2 class="h3 mb-3 font-weight-normal">Administration</h1>
                   <label for="inputEmail">Votre email</label>
-                  <input type="email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
+                  <input type="email" value="admin@test.tld" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
                   <label for="inputPassword">Mot de passe</label>
-                  <input type="password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
+                  <input type="password" value="admin" data-testid="admin-password-input" class="form-control" placeholder="******" required>
                   <button  type="submit" class="btn btn-lg btn-primary btn-block" data-testid="admin-login-button" style="background-color: #0E5AE5;">Se connecter</button>
                 </form>
               </div>
@@ -45,4 +44,3 @@ export default () => {
       </div>
   `)
 }
-  
