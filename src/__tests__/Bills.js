584132dd356abd1fcd683b8096f807dbb135741b
rayanadir <88830520+rayanadir@users.noreply.github.com>
2022-02-04 22:03:53 +0100

    mise à jour fichiers tests

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,16 +1,32 @@
-import { screen } from "@testing-library/dom"
+/**
+ * @jest-environment jsdom
+ */
+
+import { screen, waitFor } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+import { ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+
+import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
     describe("When I am on Bills Page", () => {
-        test("Then bill icon in vertical layout should be highlighted", () => {
-            const html = BillsUI({ data: [] })
-            document.body.innerHTML = html
+        test("Then bill icon in vertical layout should be highlighted", async() => {
+            Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+            window.localStorage.setItem('user', JSON.stringify({
+                type: 'Employee'
+            }))
+            const root = document.createElement("div")
+            root.setAttribute("id", "root")
+            document.body.append(root)
+            router()
+            window.onNavigate(ROUTES_PATH.Bills)
+            await waitFor(() => screen.getByTestId('icon-window'))
+            const windowIcon = screen.getByTestId('icon-window')
                 //to-do write expect expression
-            const icon_window = screen.getByTestId('icon-window');
-            const icon_activated = icon_window.classList.contains('active-icon');
-            expect(icon_activated).toBeTruthy();
+            const iconActivated = windowIcon.classList.contains('active-icon')
+            expect(iconActivated).toBeTruthy()
         })
         test("Then bills should be ordered from earliest to latest", () => {
             const html = BillsUI({ data: bills })
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,14 +1,36 @@
-import { screen } from "@testing-library/dom"
+/**
+ * @jest-environment jsdom
+ */
+
+import { screen, waitFor } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
+import { ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+
+import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    describe("When I am on NewBill Page", () => {
+        test("Then mail icon in vertical layout should be highlighted", async() => {
+            /*const html = NewBillUI()
+            document.body.innerHTML = html*/
+            //to-do write assertion
+            Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+            window.localStorage.setItem('user', JSON.stringify({
+                type: 'Employee'
+            }))
+            const root = document.createElement("div")
+            root.setAttribute("id", "root")
+            document.body.append(root)
+            router()
+            window.onNavigate(ROUTES_PATH.NewBill)
+            await waitFor(() => screen.getByTestId('icon-mail'))
+            const windowIcon = screen.getByTestId('icon-mail')
+                //to-do write expect expression
+            const iconActivated = windowIcon.classList.contains('active-icon')
+            expect(iconActivated).toBeTruthy()
+        })
     })
-  })
 })
