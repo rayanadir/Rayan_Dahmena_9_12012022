fb468cdf72d2e8adbff7f4516eebb81ffeaaf572
rayanadir <88830520+rayanadir@users.noreply.github.com>
2022-02-14 18:09:24 +0100

    correction validation/refus

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -88,7 +88,7 @@ export default class {
         if (this.counter === undefined || this.id !== bill.id) this.counter = 0
         if (this.id === undefined || this.id !== bill.id) this.id = bill.id;
         if (this.counter % 2 === 0) {
-            bills.forEach(b => {
+            filteredBills(bills, getStatus(this.id)).forEach(b => {
                 $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
             })
             $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
